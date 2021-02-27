1e58c97ba1fe3bda3f6734f07d4c4d89914fb39b
Christian <49210329+cejimenez57@users.noreply.github.com>
2021-02-27 17:03:28 -0600

    Created Login and file under components

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="main">
+        <form action="#">
+          <label>
+            First Name
+            <input type="text" />
+          </label>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
