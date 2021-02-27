8942426d7dd6d1b9d19abebfe8e935923589e6ae
Rigre Garciandia <rigrergl@outlook.com>
2021-02-27 17:12:13 -0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import MonthlyView from "./components/MonthlyView";
+import reportWebVitals from "./reportWebVitals";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/monthlyView">
+              <MonthlyView />
+            </Route>
+
+            <Route path="/">
+              <ul>
+                <li>
+                  <Link to="/monthlyView">Home</Link>
+                </li>
+              </ul>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
