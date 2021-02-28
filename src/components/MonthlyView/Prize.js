c1e2d6048338facb9efa7b9be744ee5e10e7f3e0
Rigre Garciandia <rigrergl@outlook.com>
2021-02-28 05:36:35 -0600

    milestone in automating prize buying

--- src/components/MonthlyView/Prize.js
+++ src/components/MonthlyView/Prize.js
@@ -6,75 +6,29 @@ import Button from "react-bootstrap/Button";
 import { decrementPrizes } from "../../redux/actions";
 import NumberFormat from "react-number-format";
 
-const Prize = () => {
+const Prize = ( {cost, name, src, id, buyPrize} ) => {
   return (
-    <div className="bottom-container">
-      <div className="bottom">
-        <Card style={{ width: "18rem", border: "2px solid #6B8EFF" }}>
-          <Card.Img
-            variant="top"
-            src="./images/ps5.jpg"
-            style={{ height: "50%" }}
-          />
-          <Card.Body>
-            <Card.Title>PS5</Card.Title>
+    <Card style={{ height: "45vh", width: "25vw", border: "2px solid #6B8EFF" }}>
+      <Card.Img
+        variant="top"
+        src={src}
+        style={{ height: "50%" }}
+      />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
 
-            <Card.Text>
-              <NumberFormat
-                value={25}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"$"}
-                className="prizePrice"
-              />
-            </Card.Text>
-            <Button variant="primary">Buy Now</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem", border: "2px solid #6B8EFF" }}>
-          <Card.Img
-            variant="top"
-            src="./images/nintendo.jpg"
-            style={{ height: "50%" }}
+        <Card.Text>
+          <NumberFormat
+            value={cost}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+            className="prizePrice"
           />
-          <Card.Body>
-            <Card.Title>Nintendo Switch</Card.Title>
-
-            <Card.Text>
-              <NumberFormat
-                value={25}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"$"}
-                className="prizePrice"
-              />
-            </Card.Text>
-            <Button variant="primary">Buy Now</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem", border: "2px solid #6B8EFF" }}>
-          <Card.Img
-            variant="top"
-            src="./images/xboxX.jpg"
-            style={{ height: "50%" }}
-          />
-          <Card.Body>
-            <Card.Title>Xbox Series X</Card.Title>
-
-            <Card.Text>
-              <NumberFormat
-                value={25}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"$"}
-                className="prizePrice"
-              />
-            </Card.Text>
-            <Button variant="primary">Buy Now</Button>
-          </Card.Body>
-        </Card>
-      </div>
-    </div>
+        </Card.Text>
+        <Button id={id} onClick={buyPrize} variant="primary">Buy Now</Button>
+      </Card.Body>
+    </Card>
   );
 };
 
--- src/components/MonthlyView/PrizeList.js
+++ src/components/MonthlyView/PrizeList.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Prize from "./Prize";
 
-
 export default function PrizeList() {
-  const buyPrize = (e) => {};
+  const buyPrize = (e) => {
+      console.log("Bought Prize")
+      console.log(e.target.id)
+  };
 
   let prizes = [
     {
+      id: 1,
       cost: 5000,
       name: "PS5",
       src: "./images/ps5.jpg",
       bought: false,
     },
     {
+      id: 2,
       cost: 10000,
       name: "Nintendo Switch",
       src: "./images/nintendo.jpg",
       bought: false,
     },
     {
+      id: 3,
       cost: 20000,
       name: "Xbox Series X",
       src: "./images/xboxX.jpg",
@@ -27,18 +32,16 @@ export default function PrizeList() {
   ];
 
   return (
-    <div>
-      <h1>PrizeList:</h1>
-
+    <div className="bottom">
       {prizes
         .filter((prize) => {
           if (!prize.bought) return prize;
         })
-        .map(( {cost, name, src} ) => (
-           <div>
-               <Prize cost={cost} name={name} src={src}/>
-           </div>
-          ))}
+        .map(({ cost, name, src, id }) => (
+          <div key={id}>
+            <Prize id={id} cost={cost} name={name} src={src} buyPrize={buyPrize}/>
+          </div>
+        ))}
     </div>
   );
 }
